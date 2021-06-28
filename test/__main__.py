7201be1af103855c2cf49e52935f4a2b0c472730
egg12138 <xoltraman@outlook.com>
2021-06-28 14:16:01 +0800

    完成了基本内容

--- test/__main__.py
+++ test/__main__.py
@@ -8,36 +8,45 @@ from collections import abc
 from Graphy import GraphByVertex, Vertex
 from numpy import random as rand
 from memory_profiler import *
-
+import argparse
 import matplotlib.pyplot as plt
 import cProfile
 import networkx as nx
 import numpy as np
-import sys
-
+import os.path as op
 """
-现在已经有了一个随机生成连接情况的图
-下一步是上色。
-最后可视化就再说了吧。
-
+接下来不再需要调试程序，所有就引入一个方便用的ArgParser
 """
-
-
+#TODO ArgumentParser
+#parser = argparse.ArgumentParser(description="Program To Try Four-colors-Theorom")
 color_list = ['r', 'lightcyan', 'cyan', 'c', 'teal']#Enumeration...
 
+filep = "/mnt/d/sourcecodes/git/four-colors/test/Demos/"
+def ImageSave(file:str, artist, cnt, format='png'):
+	if op.isdir:
+		filepath = op.join(file, str(cnt), '.png')
+	elif op.isfile:
+		filepath = file
+	artist.savefig(fname=filepath, format=format)
+
+
 @profile
-def main(maxVnum, maxDegNum, mode='not print'):
+def main(maxVnum, maxDegNum, mode='not print', cnt=None):
 	G= RandGenAdj(maxVnum, maxDegNum)
 	DeepFirstFillbyIndex(G, 0)
 	if mode == '--print' or mode == '-p':
 		print('*'*20)
 		print(G)
+	
 	pairs, fixed_colors = clear1(G)
 	Gnx = nx.Graph()
 	Gnx.add_edges_from(pairs)
-	nx.draw_networkx(Gnx, node_color=fixed_colors)
-	plt.show()
+	obj = nx.draw_networkx(Gnx, node_color=fixed_colors)
+	plt.savefig(op.join(filep,str(cnt)+'.png'),format='png') 
+	plt.clf()
 if __name__ == '__main__':
-	mod = sys.argv[1]
-	cProfile.run('main(20, 4, mod)')
+	mod = "--not print"
+	#cProfile.runctx('main(20, 4, mod)', None, locals())
 	#main(20, 4, sys.argv[1])
+	for i in range(20):
+		main(20, 5, mod, cnt=i)
